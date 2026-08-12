0e7cf934b5cbe972fa786a334b7d7e65d4f53cea
glendhamalta <draglendha@icloud.com>
2026-08-11 22:37:59 -0300

    Reverter: remover migração Electron/iCloud (voltar ao localStorage)

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('icloud', {
-  load: () => ipcRenderer.invoke('load-from-icloud'),
-  save: (data) => ipcRenderer.invoke('save-to-icloud', data),
-  getPath: () => ipcRenderer.invoke('get-storage-path'),
-});
